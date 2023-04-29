11fda06e3aa061b06912409865f64af611e4b0c7
alekgruszczynski <gruszal@gmail.com>
2023-04-29 14:26:12 +0200

    zajęcia 03

---
+++ zajecia03/01/zadanie/droga07.py
@@ -0,0 +1,21 @@
+def oblicz_koszt_przejazdu(dystans, cena_za_litr, spalanie_na_100km=8.5):
+    return dystans * spalanie_na_100km / 100 * cena_za_litr
+
+
+domyslna_cena = 7.97
+cena_za_litr = input(f'Podaj bieżącą cenę paliwa (ENTER aby wpisać {domyslna_cena}): ')
+if cena_za_litr == '':
+    cena_za_litr = domyslna_cena
+else:
+    cena_za_litr = float(cena_za_litr)
+
+while True:
+    dystans = float(input('Podaj dystans do pokonania: '))
+
+    koszt = oblicz_koszt_przejazdu(dystans, cena_za_litr)
+
+    print(f'Koszt przejechania {dystans}km to: {round(koszt, 2)}zł')
+
+    odpowiedz = input('Wpisz "n" jeśli chcesz zakończyć: ')
+    if odpowiedz == 'n':
+        break
---
+++ zajecia03/01/zadanie/zadanie.md
@@ -0,0 +1,11 @@
+# Zadanie
+
+## Etap 1
+Używając jako szablonu skryptu `droga07.py` napisz w nim funkcję `wczytaj_cene_paliwa` która:
+- nie będzie przyjmowała żadnego parametru
+- będzie pytała się użytkownika o cenę paliwa
+  - jeśli użytkownik naciśnie ENTER, funkcja zwróci domyślną cenę (która jest zdefiniowana na stałe w funkcji)
+  - jeśli użytkownik wpisze cenę z klawiatury, funkcja zwróci tę cenę jako obiekt typu `float`
+
+## Etap 2
+Użyj stworzonej powyżej funkcji w kodzie, aby działał tak samo, jak na początku
