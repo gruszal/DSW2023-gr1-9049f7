692d832ae97a471215e03413f8b21a2d60868630
alekgruszczynski <gruszal@gmail.com>
2023-04-29 18:40:45 +0200

    zajęcia 03

--- zajecia03/04/skrypt2.py
+++ zajecia03/04/skrypt2.py
@@ -2,8 +2,8 @@ import biblioteka2
 
 print('--- Początek skryptu')
 
-tekst = 'To jest zupełnie nowy tekst.'
+nowy_tekst = 'To jest zupełnie nowy tekst.'
 
-biblioteka2.wypisz_ilosc_znakow_w_tekscie(tekst)
+biblioteka2.wypisz_ilosc_znakow_w_tekscie(nowy_tekst)
 
 print(biblioteka2.przykladowy_tekst)
