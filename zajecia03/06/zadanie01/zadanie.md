6983775ab38c74fccc07311ef7d3369aad614454
alekgruszczynski <gruszal@gmail.com>
2023-04-30 10:53:20 +0200

    zajęcia 04

---
+++ zajecia03/06/zadanie01/zadanie.md
@@ -0,0 +1,5 @@
+# Zadanie
+
+Zmodyfikuj plik `droga09.py` tak, aby za każdym razem, gdy obliczy koszt przejechania danego dystansu, zapisywał tę informację do pliku `obliczenia.txt`.
+
+Dla każdego kosztu dopisz, dla jakiej ceny benzyny były robione obliczenia.
--- zajecia03/06/znaki/znaki03b.py
+++ zajecia03/06/znaki/znaki03b.py
@@ -1,5 +1,5 @@
 # https://docs.python.org/3/howto/unicode.html
 
-# https://unicode-table.com/en/1F602/
+# https://symbl.cc/en/1F602/
 
-print('\U0001F602')  # \U pozwala wprowadzić 32-bitową wartość (zwróć uwagę, na zera z przodu)
+print('\U0001F923')  # \U pozwala wprowadzić 32-bitową wartość (zwróć uwagę, na zera z przodu)
