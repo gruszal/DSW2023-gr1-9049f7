811325a6872253100971fa9bc3c59fb05ff7483b
alekgruszczynski <gruszal@gmail.com>
2023-05-13 10:30:43 +0200

    zajęcia 05

---
+++ zajecia05/01/zadanie_e2_rozw.py
@@ -0,0 +1,23 @@
+class Pracownik:
+    def __init__(self, imie, nazwisko, pensja):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pensja = self.ustaw_pensje(pensja)
+
+    def __repr__(self):
+        return f'Pracownik({self.imie}, {self.nazwisko})'
+
+    def ustaw_pensje(self, pensja):
+        if pensja < PLACA_MINIMALNA:
+            raise ValueError(f'Proponowana pensja "{pensja}" jest niższa niż płaca minimalna.')
+        return pensja
+
+
+if __name__ == '__main__':
+    PLACA_MINIMALNA = 3490
+
+    p1 = Pracownik('Eleonora', 'Marciniak', 5000)
+    print(p1)
+
+    p2 = Pracownik('Henryk', 'Sikorski', 3000)
+    print(p2)
